cf4f787930c577f87983a027e2fb6c0c8b6fed96
Jackson Soares <jackson.soares1994@gmail.com>
2020-03-03 10:31:53 -0300

    Primeira versao

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+"""1 Escreva um programa que obtém o salário de um funcionário, calcula e mostra o salário,
+sabendo que ele sofreu um aumento de 25%.""" 
+
+salario = float (input("Informe o seu salario: "))
+salario *= 1.25
+print("Seu salario atual é \n" + str(salario))
